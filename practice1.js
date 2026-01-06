245e93ed32df31c8b48e3fcb3aae79bb47194e1e
LovMahura <lov.mahura_cs23@gla.ac.in>
2026-01-06 09:17:44 +0530

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,19 @@
+// Write a Program for Palindrome String
+/*
+Declare a string
+check in the loop, if the reverse string is same
+if same, print output as "yes it is palindrome"
+if not, print output as "no palindrome"
+to print in js, you need console.log()
+*/
+let str = "Hello"
+let reverse = ""
+for(let i = str.length-1; i>=0; i--){
+  reverse += str[i];
+}
+if(str === reverse){
+  console.log("yes, it is a palindrome")
+}
+else{
+  console.log("no, it is not a palindrome")
+}
